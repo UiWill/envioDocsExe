1a33dfd0ac0f3df0c2da51db6ae0f37503e5bee4
Ui_WIll <williamemanoel777@gmail.com>
2025-08-20 10:40:18 -0300

    Automatico

--- src/utils/pdfProcessor.js
+++ src/utils/pdfProcessor.js
@@ -99,7 +99,12 @@ export const processPDF = async (pdfData, fileName = '') => {
            - Para documentos fiscais, use a data de vencimento
         
         4. Para NOME_PDF, identifique o tipo baseado nestas características:
-           - **PARCELAMENTO**: PRIORIDADE MÁXIMA - Se o nome do arquivo contém "PARCELAMENTO", "PARCELA", "PARCELADO" OU se o conteúdo menciona parcelamento de tributos OU se o documento contém código da receita 1124 (parcelamento INSS) OU número de referência começando com "021100" (parcelamento), USE EXATAMENTE o nome do arquivo SEM a extensão .pdf (ex: se arquivo é "PARCELAMENTO_ICMS.pdf" use "PARCELAMENTO_ICMS", se é "PARCELAMENTO_INSS.pdf" use "PARCELAMENTO_INSS")
+           - **PARCELAMENTO**: PRIORIDADE MÁXIMA - Se o nome do arquivo contém "PARCELAMENTO", "PARCELA", "PARCELADO" OU se o conteúdo menciona parcelamento de tributos OU se o documento contém código da receita 1124 (parcelamento INSS) OU número de referência começando com "021100" (parcelamento), extraia apenas a parte relevante do nome do arquivo:
+             * Se arquivo contém "PARCELAMENTO INSS" ou "PARCELAMENTO IINSS", use "PARCELAMENTO INSS"
+             * Se arquivo contém "PARCELAMENTO ICMS", use "PARCELAMENTO ICMS"
+             * Se arquivo contém "PARCELAMENTO SIMPLES", use "PARCELAMENTO SIMPLES"
+             * Se arquivo contém apenas "PARCELAMENTO", use "PARCELAMENTO"
+             * IGNORE palavras como "CAIU COMO DARF", "TESTE", "COPIA", etc.
            - **PGDAS**: PRIORIDADE MUITO ALTA - Se o documento contém "Documento de Arrecadação do Simples Nacional" OU "SIMPLES NACIONAL" OU se menciona códigos como "IRPJ - SIMPLES NACIONAL", identifique como "PGDAS"
            - **DARF**: PRIORIDADE ALTA - Se o documento contém "Documento de Arrecadação de Receitas Federais" OU "DARF" OU se o nome do arquivo contém "DCTFWEB", identifique como "DARF"
            - FGTS: Guia de Recolhimento do FGTS ou GRF Digital
